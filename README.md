e15dd28849350610aec6e6e6a41b57fcbb75b7be
alexandresillva <alexandresillva@gmail.com>
2020-10-05 13:57:38 -0300

    Final implemetation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # django_rest_framework
 This project is about how to create and implement an API  with a Django Rest Framework.
+ok
--- core/api/serializers.py
+++ core/api/serializers.py
@@ -2,18 +2,23 @@ from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
+from comentarios.api.serializers import ComentarioSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
 from rest_framework.fields import SerializerMethodField
 
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer()
+    comentarios = ComentarioSerializer()
+    avaliacoes = AvaliacaoSerializer()
     descricao_completa = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = ['id', 'nome', 'descricao', 'aprovado', 'foto', 'descricao_completa', 'atracoes', 'endereco',
-                  'descricao_completa2']
+                  'comentarios', 'avaliacoes', 'descricao_completa2']
+    read_only_fields = ('atracoes', 'endereco')
 
     @staticmethod
     def get_descricao_completa(obj):
